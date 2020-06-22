647bbc7dda59080347da8939fce755c5e2c4a6ed
Евгений Малашкин <eugene.malashkin@gmail.com>
2020-06-23 01:14:33 +0300

    Podelie

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = "Hello World";
+const lines = [];
+const addLine = (text, color) => {
+  const line = {
+    colorClass: color ?? "black",
+    text
+  };
+  lines.push(line);
+};
+
+const expect = actual => ({
+  toBe(expected) {
+    if (actual !== expected) {
+      addLine(`Actual value ${actual} is not an expected ${expected}`, "red");
+    } else {
+      addLine("Passed");
+    }
+  }
+});
+
+expect(2).toBe(2);
+
+document.getElementById("app").innerHTML = lines
+  .map(line => `<div class="${line.colorClass}">${line.text}</div>`)
+  .join("");
